bcd7fd60d7cb5a3c43909ab40350816b4465a2a9
aldolushkja <aldo.lushkja@gmail.com>
2021-08-02 21:59:06 +0200

    improved logging interceptor information against class injected

--- src/main/java/com/aldolushkja/books_store/interceptors/LoggingInterceptor.java
+++ src/main/java/com/aldolushkja/books_store/interceptors/LoggingInterceptor.java
@@ -18,11 +18,11 @@ public class LoggingInterceptor {
     public Object logEntryMethod(InvocationContext ctx) throws Exception {
         long startTime = System.currentTimeMillis();
         try {
-            LOGGER.info("Entering method: " + ctx.getMethod().getName());
+            LOGGER.info("Entering method: " + ctx.getTarget().getClass().getName() + "." + ctx.getMethod().getName());
             return ctx.proceed();
         } finally {
             long endtime = System.currentTimeMillis();
-            LOGGER.info("Exit method: " + ctx.getMethod().getName() + ". Execution took " + (endtime - startTime) + " ms.");
+            LOGGER.info("Exit method: " + ctx.getTarget().getClass().getName() + "." + ctx.getMethod().getName() + ". Execution took " + (endtime - startTime) + " ms.");
         }
     }
 }
